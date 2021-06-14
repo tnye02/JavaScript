5be9dff6dcc33d1c4f2670c77d1908a018764f43
tnye02 <tnye02@gmail.com>
2021-06-14 11:11:59 -0700

    New JavaScript Repository

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+
+
+var B = "I love to display text as an alert " + "as well as in the browser! ", A = "The \"HELLO WORLD!!\" string!" + " This is the second part of the string! ";  //concatenating two strings
+
+var A = A.fontcolor("red"), B = B.fontcolor("green"); //setting font color for both strings
+
+var C = A + B; //concatenate the two strings with a new variable
+
+window.alert("Hello world!");
+
+document.write(C); //displaying the two strings into the browser window
+
+document.write(1000 + 283948); //displaying a simple math expression
